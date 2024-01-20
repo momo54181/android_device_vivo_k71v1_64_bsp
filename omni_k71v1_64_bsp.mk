1c8685e51515d89369387e180bcbe5b8b36addfa
Momo <momo5418@outlook.com>
2024-01-20 19:32:18 +0800

    back

--- omni_k71v1_64_bsp.mk
+++ omni_k71v1_64_bsp.mk
@@ -1,29 +1,38 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from k71v1_64_bsp device
 $(call inherit-product, device/vivo/k71v1_64_bsp/device.mk)
 
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := k71v1_64_bsp
 PRODUCT_NAME := omni_k71v1_64_bsp
 PRODUCT_BRAND := vivo
-PRODUCT_MODEL := vivo 1806
+PRODUCT_MODEL := vivo
 PRODUCT_MANUFACTURER := vivo
-
-PRODUCT_GMS_CLIENTID_BASE := android-vivo-rev1
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="full_k71v1_64_bsp-user 10 QP1A.190711.020 compiler12301213 release-keys"
-
-BUILD_FINGERPRINT := vivo/1806/1806:10/QP1A.190711.020/compiler12301213:user/release-keys
+PRODUCT_RELEASE_NAME := vivo vivo

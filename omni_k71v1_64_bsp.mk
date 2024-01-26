7dd0be6315613882f26b2722b0b3da0242e4754f
Momo <momo5418@outlook.com>
2024-01-26 16:49:52 +0800

    fix common.mk

--- omni_k71v1_64_bsp.mk
+++ omni_k71v1_64_bsp.mk
@@ -7,7 +7,9 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)

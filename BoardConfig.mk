b2f09e259fdfdfefd390e7981386067090876eb8
Momo5418 <momo5418@mocn.top>
2024-06-30 15:19:53 +0800

    Update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,10 +111,7 @@ TW_DEFAULT_BRIGHTNESS := 2000
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/bootdevice/by-name/userdata"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
+TW_INCLUDE_CRYPTO_FBE := true
 
 # Debug
 TWRP_INCLUDE_LOGCAT := true

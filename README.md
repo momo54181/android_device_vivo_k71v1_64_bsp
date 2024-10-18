93005a184c529b8a5a2aa98b46f0ae8499a1f8cf
Momo5418 <momo5418@mocn.top>
2024-10-18 19:12:04 +0800

    Update

--- README.md
+++ README.md
@@ -1,10 +1,35 @@
-# Android device tree for vivo vivo 1806 (k71v1_64_bsp)
+# Android device tree for vivo V11i Z3i Y97 (k71v1_64_bsp)
+==========================================
 
-```
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-```
+## Status
+
+**Working**:
+
+1. Decryption
+2. Backup and Restore
+3. Flash Stock ROM
+4. Flash GSI or Custom ROMs
+5. Screenshots now renders correct color
+6. MTP
+
+**Not tested**:
+
+1. OTG Works
+
+## Downloads
+
+https://github.com/momo54181/android_device_vivo_k71v1_64_bsp/releases
+
+### Device specifications
+
+| Device                  |         Vivo V11i Z3i Y97                                     |
+| ----------------------: | :------------------------------------------------------------ |
+| SoC                     | MediaTek Helio P60 (MT6771)                                   |
+| CPU                     | 4x 2.0 GHz ARM Cortex-A73, 4x 2.0 GHz ARM Cortex-A53          |
+| GPU                     | Arm Mali-G72, 700MHz                                          |
+| Memory                  | 4 GB, 6 GB, 1800 MHz                                          |
+| Shipped Android version | Android 8.1,upgradable to Android 10.0                        |
+| Storage                 | 64 GB, 128 GB                                                 |
+| Battery                 | Non-removable Li-Ion 3240 mAh battery                         |
+| Dimensions              | 75.63 mm x 155.97 mm x 8.1 mm                                 |
+| Display                 | 6.3 in, IPS, 1080 x 2280 pixels                               |
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,65 @@
-#
 # Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024 OrangeFox Recovery Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-add_lunch_combo omni_k71v1_64_bsp-user
-add_lunch_combo omni_k71v1_64_bsp-userdebug
-add_lunch_combo omni_k71v1_64_bsp-eng
+FDEVICE="PACM00"
+#set -o xtrace
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+
+# let's see what are our build VARs
+# Initial Exports
+	export ALLOW_MISSING_DEPENDENCIES=true
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+    export LC_ALL="C"
+
+# Version / Maintainer infos.
+	export OF_MAINTAINER="Momo5418"
+	export FOX_VERSION="R11.1"
+	export FOX_BUILD_TYPE="Unofficial-Stable"
+
+# Custom pic for the maintainer's info in about section
+	export OF_MAINTAINER_AVATAR="/device/vivo/k71v1_64_bsp/Maintainer.png"
+	
+# Device Information.
+	export FOX_ARCH=arm64
+	export FOX_VARIANT="A10_FDE"
+	export TARGET_DEVICE_ALT="PD1813" # I am not what are the over possible names.
+    export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
+
+    # export OF_SKIP_FBE_DECRYPTION_SDKVERSION= 30
+    export OF_DEFAULT_KEYMASTER_VERSION=3.0
+    export OF_FLASHLIGHT_ENABLE=0
+
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+	  export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+	  export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+	  export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+	  export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+	fi
+fi		
